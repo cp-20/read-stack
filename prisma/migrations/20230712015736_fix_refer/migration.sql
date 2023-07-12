21e14181e889beea574ddab3691dde0122ee5f35
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-07-12 10:57:42 +0900

    🚧 DB定義がちょっと間違ってたので修正

---
+++ prisma/migrations/20230712015736_fix_refer/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `articleId` on the `article` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "article" DROP COLUMN "articleId";
