36050973f7298fc58da3f9915f76907cc15e73c9
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-08-10 15:33:11 +0900

    🚧 `GET /users/[id]/clips` をいい感じに

--- packages/web/src/server/handlers/users/[id]/clips/get.ts
+++ packages/web/src/server/handlers/users/[id]/clips/get.ts
@@ -2,9 +2,12 @@ import type { NextApiHandler } from 'next';
 import { z } from 'zod';
 import { prisma } from '@/features/database/prismaClient';
 import { requireAuthWithUserMiddleware } from '@/server/middlewares/authorize';
+import { parseInt } from '@/shared/lib/parseInt';
 
 const getUserClipsSchema = z.object({
   id: z.string(),
+  cursor: z.string().optional(),
+  limit: z.string().optional(),
 });
 
 export const getUserClips: NextApiHandler = requireAuthWithUserMiddleware()(
@@ -14,11 +17,24 @@ export const getUserClips: NextApiHandler = requireAuthWithUserMiddleware()(
       return res.status(400).json({ error: query.error });
     }
     const { id } = query.data;
+    const cursor = parseInt(query.data.cursor, -1);
+    const limit = parseInt(query.data.limit, 20);
+
+    const cursorOption = cursor !== -1 ? { cursor: { id: cursor } } : undefined;
 
     const clips = await prisma.clips.findMany({
       where: {
         authorId: id,
       },
+      orderBy: {
+        updatedAt: 'desc',
+      },
+      include: {
+        article: true,
+      },
+      ...cursorOption,
+      take: Math.min(100, limit),
+      skip: cursor !== undefined ? 1 : 0,
     });
 
     return res.status(200).json({ clips });
