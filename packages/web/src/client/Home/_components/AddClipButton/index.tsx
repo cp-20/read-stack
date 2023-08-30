03cf46661a9f3377e41f080fc5eea36a14b3eb92
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-08-30 15:15:21 +0900

    🚧 記事の追加時のアニメーションを設定

--- packages/web/src/client/Home/_components/AddClipButton/index.tsx
+++ packages/web/src/client/Home/_components/AddClipButton/index.tsx
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react';
 import { Button, Group, Popover, TextInput } from '@mantine/core';
 import type { FormEventHandler } from 'react';
-import { useState, type FC } from 'react';
+import { useState, type FC, useCallback } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import { useAddClip } from '@/client/_components/hooks/useUserClips';
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,37 +9,44 @@ import 'react-toastify/dist/ReactToastify.css';
 export const AddClipButton: FC = () => {
   const { addClip } = useAddClip();
   const [url, setUrl] = useState('');
+  const [submitting, setSubmitting] = useState(false);
 
-  const handleSubmit: FormEventHandler = async (e) => {
-    e.preventDefault();
-    if (url === '') return;
+  const handleSubmit: FormEventHandler = useCallback(
+    async (e) => {
+      e.preventDefault();
+      if (url === '') return;
 
-    const result = await addClip(url);
+      setSubmitting(true);
 
-    if (!result) {
-      return toast('新しい記事の追加に失敗しました', {
-        type: 'error',
-      });
-    }
+      const result = await addClip(url);
 
-    toast(
-      <>
-        新しい記事「
-        <span
-          css={css`
-            font-weight: bold;
-          `}
-        >
-          {result.article.title}
-        </span>
-        」を追加しました
-      </>,
-      {
-        type: 'success',
-      },
-    );
-    setUrl('');
-  };
+      if (!result) {
+        return toast('新しい記事の追加に失敗しました', {
+          type: 'error',
+        });
+      }
+
+      toast(
+        <>
+          新しい記事「
+          <span
+            css={css`
+              font-weight: bold;
+            `}
+          >
+            {result.article.title}
+          </span>
+          」を追加しました
+        </>,
+        {
+          type: 'success',
+        },
+      );
+      setUrl('');
+      setSubmitting(false);
+    },
+    [addClip, url],
+  );
 
   return (
     <>
@@ -54,8 +61,11 @@ export const AddClipButton: FC = () => {
                 placeholder="記事のURL"
                 value={url}
                 onChange={(e) => setUrl(e.target.value)}
+                disabled={submitting}
               />
-              <Button type="submit">追加</Button>
+              <Button type="submit" loading={submitting}>
+                追加
+              </Button>
             </Group>
           </form>
         </Popover.Dropdown>
