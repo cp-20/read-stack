c577b5bc91c5d35fc7ff503b9621257231b6c7f6
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-07 19:23:05 +0900

    🚧 `PATCH /users/:id/clips/:clipId`を修正

--- packages/web/src/server/handlers/users/[id]/clips/[clipId]/patch.ts
+++ packages/web/src/server/handlers/users/[id]/clips/[clipId]/patch.ts
@@ -4,8 +4,11 @@ import { z } from 'zod';
 import { prisma } from '@/features/database/prismaClient';
 import { requireAuthWithUserMiddleware } from '@/server/middlewares/authorize';
 
-const updateUserClipByIdSchema = z.object({
+const updateUserClipByIdQuerySchema = z.object({
   clipId: z.number().int(),
+});
+
+const updateUserClipByIdBodySchema = z.object({
   clip: z
     .object({
       status: z.number().int().min(0).max(2),
@@ -17,18 +20,23 @@ const updateUserClipByIdSchema = z.object({
 
 export const updateUserClipById: NextApiHandler =
   requireAuthWithUserMiddleware()(async (req, res) => {
-    const query = updateUserClipByIdSchema.safeParse(req.query);
+    const query = updateUserClipByIdQuerySchema.safeParse(req.query);
     if (!query.success) {
       return res.status(400).json({ error: query.error });
     }
-    const { clipId, clip: clipData } = query.data;
+    const body = updateUserClipByIdBodySchema.safeParse(req.body);
+    if (!body.success) {
+      return res.status(400).json({ error: body.error });
+    }
+    const { clipId } = query.data;
+    const { clip: patchClip } = body.data;
 
     try {
       const clip = await prisma.clips.update({
         where: {
           id: clipId,
         },
-        data: clipData,
+        data: patchClip,
       });
       return res.status(200).json({ clip });
     } catch (err) {
