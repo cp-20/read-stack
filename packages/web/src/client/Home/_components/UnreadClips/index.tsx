1ad6d91d39a04a3a455151278fd174f35b3a90eb
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-08 17:52:30 +0900

    🚧 既読の記事を含めるか & カード形式かリスト形式かを選べるように

--- packages/web/src/client/Home/_components/UnreadClips/index.tsx
+++ packages/web/src/client/Home/_components/UnreadClips/index.tsx
@@ -1,17 +1,26 @@
 import { Center, Loader } from '@mantine/core';
 import { useIntersection } from '@mantine/hooks';
-import { useEffect, type FC } from 'react';
+import type { ReactNode, FC } from 'react';
+import { useEffect } from 'react';
 import { UnreadClipList } from './UnreadClipList';
 import { NoContent } from '@/client/Home/_components/UnreadClips/NoContent';
+import { UnreadClipPanels } from '@/client/Home/_components/UnreadClips/UnreadClipPanels';
 import { useUserClips } from '@/client/_components/hooks/useUserClips';
 
-export const UnreadClips: FC = () => {
+export type UnreadClipViewType = 'panel' | 'list';
+
+export type UnreadClipsProps = {
+  type: UnreadClipViewType;
+  includeRead?: boolean;
+};
+
+export const UnreadClips: FC<UnreadClipsProps> = ({ type, includeRead }) => {
   const { ref, entry } = useIntersection({
     threshold: 0,
   });
 
   const { clips, loadNext, isLoading } = useUserClips({
-    query: { unreadOnly: true },
+    query: { unreadOnly: !includeRead },
   });
 
   useEffect(() => {
@@ -20,9 +29,16 @@ export const UnreadClips: FC = () => {
     }
   }, [entry?.isIntersecting, loadNext]);
 
+  const views: Record<UnreadClipViewType, ReactNode> = {
+    panel: <UnreadClipPanels clips={clips} />,
+    list: <UnreadClipList clips={clips} />,
+  };
+
+  const UnreadClipView = views[type];
+
   return (
     <>
-      <UnreadClipList clips={clips} />
+      {UnreadClipView}
       {!isLoading && clips.length === 0 && <NoContent />}
       {isLoading && (
         <Center mt={16} ref={ref}>
--- packages/web/src/client/Home/index.tsx
+++ packages/web/src/client/Home/index.tsx
@@ -1,13 +1,24 @@
 import { css } from '@emotion/react';
-import { useMantineTheme } from '@mantine/core';
+import {
+  Checkbox,
+  Flex,
+  SegmentedControl,
+  useMantineTheme,
+} from '@mantine/core';
+import { IconBook2 } from '@tabler/icons-react';
 import type { NextPage } from 'next';
 
+import { useState } from 'react';
 import { Status } from './_components/Status';
+import type { UnreadClipViewType } from './_components/UnreadClips';
 import { UnreadClips } from './_components/UnreadClips';
 import { AddClipButton } from '@/client/Home/_components/AddClipButton';
 
 export const Home: NextPage = () => {
   const theme = useMantineTheme();
+  const [includeRead, setIncludeRead] = useState(false);
+  const [viewType, setViewType] = useState<UnreadClipViewType>('panel');
+
   return (
     <>
       <div
@@ -19,13 +30,35 @@ export const Home: NextPage = () => {
         <Status />
         <h2
           css={css`
+            display: flex;
+            align-items: end;
+            justify-content: space-between;
             border-bottom: 1px solid #ccc;
             margin-bottom: 1rem;
           `}
         >
-          未読の記事一覧
+          <div>未読の記事一覧</div>
+          <Flex align="center" gap={16}>
+            <Checkbox
+              icon={IconBook2}
+              checked={includeRead}
+              onChange={(e) => setIncludeRead(e.currentTarget.checked)}
+              aria-label="既に読んだ記事を含めるかどうか"
+              title="既に読んだ記事を含めるかどうか"
+              indeterminate
+              size="lg"
+            />
+            <SegmentedControl
+              value={viewType}
+              onChange={(type) => setViewType(type as UnreadClipViewType)}
+              data={[
+                { label: 'Card', value: 'panel' },
+                { label: 'List', value: 'list' },
+              ]}
+            />
+          </Flex>
         </h2>
-        <UnreadClips />
+        <UnreadClips type={viewType} includeRead={includeRead} />
         <div
           css={css`
             position: fixed;
