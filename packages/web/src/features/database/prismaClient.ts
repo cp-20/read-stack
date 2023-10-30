ce491a380a6b25093e20f65d1303d401c180e38c
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-10-30 18:43:32 +0900

    🚧 logging

--- packages/web/src/features/database/prismaClient.ts
+++ packages/web/src/features/database/prismaClient.ts
@@ -1,3 +1,28 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+});
+
+prisma.$on('query', (e) => {
+  console.log(
+    `Query: ${e.query}\nParams: ${e.params}\nDuration: ${e.duration}ms`,
+  );
+});
