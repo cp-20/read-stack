71630e4a594a9e7e964fa79f2af74e7dfbc792a5
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-12-28 23:30:19 +0900

    husky & lint-staged やめる

--- .lintstagedrc.mjs
+++
@@ -1,3 +0,0 @@
-export default {
-	"*.{js,jsx,ts,tsx}": "biome format --write",
-};
--- apps/server/.lintstagedrc.mjs
+++
@@ -1,4 +0,0 @@
-export default {
-	"*.{js,jsx,ts,tsx}": "biome format --write",
-	"*.{js,jsx,ts,tsx}": "pnpm lint",
-};
