f421c8f5aeda071e545c53a50465380f26b9ac4e
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-12-29 23:42:43 +0900

    update dependencies

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,75 +1,74 @@
-import { fetcher } from '@/features/swr/fetcher';
-import { GoogleTagManagerBody } from '@/shared/components/GoogleTagManager';
-import '@/shared/styles/global.css';
-import { MantineProvider } from '@mantine/core';
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
-import type { Session } from '@supabase/auth-helpers-react';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import type { AppProps } from 'next/app';
-import { Noto_Sans_JP as fontNotoSansJP } from 'next/font/google';
-import { useMemo } from 'react';
-import 'ress';
-import { SWRConfig } from 'swr';
+import { fetcher } from "@/features/swr/fetcher";
+import { GoogleTagManagerBody } from "@/shared/components/GoogleTagManager";
+import "@/shared/styles/global.css";
+import { MantineProvider } from "@mantine/core";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import type { Session } from "@supabase/auth-helpers-react";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import type { AppProps } from "next/app";
+import { Noto_Sans_JP as fontNotoSansJP } from "next/font/google";
+import { useMemo } from "react";
+import "ress";
+import { SWRConfig } from "swr";
 
 const font = fontNotoSansJP({
-  weight: ['400', '600'],
-  subsets: ['latin', 'latin-ext', 'cyrillic'],
-  preload: true,
+	weight: ["400", "600"],
+	subsets: ["latin", "latin-ext", "cyrillic"],
+	preload: true,
 });
 
 const authCookieName = process.env.NEXT_PUBLIC_SUPABASE_AUTH_COOKIE_NAME;
 
 const MyApp = ({
-  Component,
-  pageProps,
+	Component,
+	pageProps,
 }: AppProps<{ initialSession: Session }>) => {
-  const supabaseClient = useMemo(
-    () =>
-      createPagesBrowserClient({
-        cookieOptions: authCookieName
-          ? {
-              name: authCookieName,
-              secure: true,
-              sameSite: 'Lax',
-              domain: '',
-              maxAge: '86400',
-              path: '/',
-            }
-          : undefined,
-      }),
-    [],
-  );
+	const supabaseClient = useMemo(
+		() =>
+			createPagesBrowserClient({
+				cookieOptions: authCookieName
+					? {
+							name: authCookieName,
+							secure: true,
+							sameSite: "Lax",
+							domain: "",
+							path: "/",
+					  }
+					: undefined,
+			}),
+		[],
+	);
 
-  return (
-    <>
-      <GoogleTagManagerBody />
-      <MantineProvider
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'light',
-          breakpoints: {
-            xs: '360px',
-            sm: '540px',
-            md: '720px',
-            lg: '1080px',
-            xl: '1200px',
-          },
-          fontFamily: font.style.fontFamily,
-        }}
-        withGlobalStyles
-        withNormalizeCSS
-      >
-        <SessionContextProvider
-          initialSession={pageProps.initialSession}
-          supabaseClient={supabaseClient}
-        >
-          <SWRConfig value={{ fetcher }}>
-            <Component {...pageProps} />
-          </SWRConfig>
-        </SessionContextProvider>
-      </MantineProvider>
-    </>
-  );
+	return (
+		<>
+			<GoogleTagManagerBody />
+			<MantineProvider
+				theme={{
+					/** Put your mantine theme override here */
+					colorScheme: "light",
+					breakpoints: {
+						xs: "360px",
+						sm: "540px",
+						md: "720px",
+						lg: "1080px",
+						xl: "1200px",
+					},
+					fontFamily: font.style.fontFamily,
+				}}
+				withGlobalStyles
+				withNormalizeCSS
+			>
+				<SessionContextProvider
+					initialSession={pageProps.initialSession}
+					supabaseClient={supabaseClient}
+				>
+					<SWRConfig value={{ fetcher }}>
+						<Component {...pageProps} />
+					</SWRConfig>
+				</SessionContextProvider>
+			</MantineProvider>
+		</>
+	);
 };
 
 export default MyApp;
