b49b51b7da595c29c72a1b4f2e579e1ca4225718
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-01-17 09:04:30 +0900

    format

--- apps/web/src/client/Home/_components/ArticleListModel/UnreadClipList.tsx
+++ apps/web/src/client/Home/_components/ArticleListModel/UnreadClipList.tsx
@@ -228,26 +228,26 @@ const AddNewClipForm: FC = () => {
   return (
     <form
       css={css`
-      padding: 1rem;
-      border: 1px solid ${theme.colors.gray[2]};
-      border-radius: ${theme.radius.md};
-      margin-bottom: 1rem;
+        padding: 1rem;
+        border: 1px solid ${theme.colors.gray[2]};
+        border-radius: ${theme.radius.md};
+        margin-bottom: 1rem;
       `}
       onSubmit={handleSubmit}
     >
       <Text
         css={css`
-      margin-bottom: 0.5rem;
-      `}
+          margin-bottom: 0.5rem;
+        `}
       >
         URLを入力して記事を追加
       </Text>
       <div
         css={css`
-      display: grid;
-      gap: 0.5rem;
-      grid-template-columns: 1fr auto;
-      `}
+          display: grid;
+          gap: 0.5rem;
+          grid-template-columns: 1fr auto;
+        `}
       >
         <TextInput
           onChange={(e) => {
