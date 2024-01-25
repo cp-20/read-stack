6ea6de720430be83ce7ae24392d39b19b2bac392
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-01-26 04:09:48 +0900

    make immutable

--- apps/web/src/client/_components/hooks/useUser.ts
+++ apps/web/src/client/_components/hooks/useUser.ts
@@ -1,12 +1,16 @@
 import { fetcher } from '@/features/swr/fetcher';
 import type { getUserResponseSchema } from '@read-stack/openapi';
-import useSWR from 'swr';
+import useImmutableSWR from 'swr/immutable';
 import type { z } from 'zod';
 
 type GetMeResponse = z.infer<typeof getUserResponseSchema>;
 
 export const useUser = () => {
-  const { data, error } = useSWR<GetMeResponse>('/api/v1/users/me', fetcher);
+  const { data, error } = useImmutableSWR<GetMeResponse>(
+    '/api/v1/users/me',
+    fetcher,
+    {},
+  );
 
   return { user: data?.user, error };
 };
