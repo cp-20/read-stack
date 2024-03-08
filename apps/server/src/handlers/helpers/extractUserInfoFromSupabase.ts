9b8894fd77c01fc969a37b2aaa014918f6072b80
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-03-09 04:00:18 +0900

    apple providerに対応

--- apps/server/src/handlers/helpers/extractUserInfoFromSupabase.ts
+++ apps/server/src/handlers/helpers/extractUserInfoFromSupabase.ts
@@ -20,6 +20,15 @@ export const extractUserInfoFromSupabase = (user: User) => {
         displayName: user.user_metadata.name as string,
         avatarUrl: user.user_metadata.avatar_url as string,
       };
+    case 'apple':
+      return {
+        id: user.id,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- たぶん大丈夫
+        email: user.email!,
+        name: 'user',
+        displayName: 'user',
+        avatarUrl: user.user_metadata.avatar_url as string,
+      };
     default:
       throw new Error('invalid provider');
   }
